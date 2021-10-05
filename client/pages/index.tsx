df1d06ca87b8f01628556d577b2f9be8129acc90
diebraga <diebraga0055@gmail.com>
2021-10-06 00:44:23 +0100

    first commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home">
+    <h1>Hello world</h1>
+  </Layout>
+)
+
+export default IndexPage
